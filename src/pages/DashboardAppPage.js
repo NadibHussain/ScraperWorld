f7b2dc0aa2066a55d0f0c342d30a374fb946222d
NadibHussain <nadib2015@gmail.com>
2023-07-28 18:25:01 -0400

    action test1

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -35,7 +35,7 @@ export default function DashboardAppPage() {
 
       <Container maxWidth="xl">
         <Typography variant="h4" sx={{ mb: 5 }}>
-          Hi, Welcome back {'dasdas'}
+          Hi, Welcome back {'User'}
         </Typography>
 
         <Grid container spacing={3}>
